1bde5ee912d4beec6c4feea5e80cccc77beb54e0
Jon Hoffman <jonhoffman@gmail.com>
2010-06-06 18:07:01 -0400

    Using a lighter weight data structure to keep track of visited objects

--- src/main/java/org/jh/GraphTraverser.java
+++ src/main/java/org/jh/GraphTraverser.java
@@ -9,13 +9,12 @@ import java.util.Map;
 
 
 public class GraphTraverser {
-	private static final Object MARKER = new Object();
 	
 	private final Map<Class<?>, ClassInfo> _classMap = new HashMap<Class<?>, ClassInfo>();
-	private final IdentityHashMap<Object, Object> _visited = new IdentityHashMap<Object, Object>();
+	private final IdentitySet _visited = new LiteIdentitySet();
 	
 	private boolean shouldFollow(Object o) {
-		return o != null && _visited.put(o, MARKER) == null;
+		return o != null && _visited.add(o);
 	}
 	
 	private ClassInfo getClassInfo(Object obj) {
@@ -77,4 +76,80 @@ public class GraphTraverser {
 		
 	}
 	
+	private interface IdentitySet {
+		boolean add(Object o);
+	}
+	
+	private static class IdentityHashSet implements IdentitySet {
+		private static final Object MARKER = new Object();
+		private final IdentityHashMap<Object, Object> _set = new IdentityHashMap<Object, Object>();
+		public boolean add(Object o) {
+			return _set.put(o, MARKER) == null;
+		}
+	}
+	
+	
+	/*
+	 * Some of this code was borrowed from http://code.google.com/p/ontopia/source/browse/trunk/ontopia/src/java/net/ontopia/utils/CompactHashSet.java
+	 * (it's apache 2.0 licensed)
+	 */
+	private static class LiteIdentitySet implements IdentitySet {
+		private static final float LOAD_FACTOR = 0.8f;
+		private static final int SIZE = 10;
+
+		protected int elements = 0;
+		protected Object[] objects = new Object[SIZE];
+
+		public boolean add(Object o) {
+			int index = hash(o) % objects.length;
+			int offset = 1;
+		
+			while (objects[index] != null && !(objects[index].equals(o))) {
+				index = ((index + offset) & 0x7FFFFFFF) % objects.length;
+				offset = offset * 2 + 1;
+		
+				if (offset == -1)
+					offset = 2;
+			}
+		
+			if (objects[index] == null) {
+
+				// rehash and grow
+				if ((elements / (double) objects.length) > LOAD_FACTOR) {
+					rehash((objects.length << 1) + 1);
+				}
+				elements++;
+				objects[index] = o;
+				
+				return true;
+			} else return false;
+		}
+		
+		private void rehash(int newSize) {
+			Object[] newObjects = new Object[newSize];
+
+			for (int i = 0; i < objects.length; i++) {
+				Object o = objects[i];
+
+				int index = hash(o) % newSize;
+				int offset = 1;
+
+				while (newObjects[index] != null) {
+					index = ((index + offset) & 0x7FFFFFFF) % newSize;
+					offset = offset * 2 + 1;
+
+					if (offset == -1)
+						offset = 2;
+				}
+
+				newObjects[index] = o;
+			}
+
+			objects = newObjects;
+		}
+		
+		private int hash(Object obj) {
+		    return System.identityHashCode(obj) & 0x7FFFFFFF;
+		}
+    }
 }
